d78cee7320dbc393baa5a7bf6c41ccb516674519
DevSIMPLIMS <tcc2nanuvem@gmail.com>
2025-08-25 04:06:03 -0400

    criacao do model matriz,html de listar matriz e forms.py, com o MatrizForm

--- simplims_app/admin.py
+++ simplims_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Matriz
 
 # Register your models here.
+admin.site.register(Matriz)
---
+++ simplims_app/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+from .models import Matriz
+
+class MatrizForm(forms.ModelForm):
+
+    class Meta:
+        model = Matriz
+        fields = [
+            'descricao',
+        ]
