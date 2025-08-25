c86f467bd2776bb06fde203239937da7a57ee801
DevSIMPLIMS <tcc2nanuvem@gmail.com>
2025-08-25 04:23:14 -0400

    criacao da classe DeleteRecordMixin no arquivo mixins.py

--- simplims_app/mixins.py
+++ simplims_app/mixins.py
@@ -4,8 +4,18 @@ from .forms import MatrizForm
 
 class MatrizViewMixin:
     """
-    Mixins para views de Matriz: define model, form e URL de sucesso.
+    Mixin para views de Matriz: define model, form e URL de sucesso.
     """
     model = Matriz
     form_class = MatrizForm
     success_url = reverse_lazy("lista_matrizes")
+
+
+class DeleteRecordMixin:
+    """
+       Mixin para adicionar lógica extra em DeleteView (mensagens, logs, etc.)
+    """
+    def delete(self, request, *args, **kwargs):
+        # Aqui você pode adicionar lógica comum, como log ou mensagens
+        print(f"Registro será deletado: {self.object}")
+        return super().delete(request, *args, **kwargs)
