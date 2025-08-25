7410463b8a2d158462f3206717b716f9cc2e0b47
DevSIMPLIMS <tcc2nanuvem@gmail.com>
2025-08-25 01:19:51 -0400

    criação das páginas html base, navbar e home.

--- simplims_app/views.py
+++ simplims_app/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime, timedelta
 
-# Create your views here.
+def home(request, ano=datetime.now().year, mes=datetime.now().strftime('%B')):
+    nome = "Andrimarciely"
+    mes = mes.capitalize()
+
+    # converte o nome do mês em número correspondente
+    numero_mes = list(calendar.month_name).index(mes)
+    numero_mes = int(numero_mes)
+
+    # Create a calendar
+    cal = HTMLCalendar().formatmonth(ano, numero_mes, withyear = True)
+
+    # Get current year
+    agora = datetime.now()
+    ano_corrente = agora.year
+
+    # Get current time
+    horario = agora.strftime('%H:%M')
+
+    return render(request,'home.html',{
+        "nome": nome,
+        "ano": ano,
+        "mes": mes,
+        "numero_mes": numero_mes,
+        "cal": cal,
+        "ano_corrente": ano_corrente,
+        "horario": horario,
+        })
