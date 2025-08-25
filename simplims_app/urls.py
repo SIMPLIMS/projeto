ef97efa135affa8901ecb289abc62c012449bf4d
DevSIMPLIMS <tcc2nanuvem@gmail.com>
2025-08-25 04:38:15 -0400

    criacao das urls para o model Matriz

--- simplims_app/urls.py
+++ simplims_app/urls.py
@@ -2,4 +2,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.home, name='home'),
+    # URLs da Matriz
+    path('matrizes/', views.MatrizListView.as_view(), name='lista_matrizes'),
+    path('matrizes/novo/', views.MatrizCreateView.as_view(), name='criar_matriz'),
+    path('matrizes/<int:pk>/editar/', views.MatrizUpdateView.as_view(), name='editar_matriz'),
+    path('matrizes/<int:pk>/excluir/', views.MatrizDeleteView.as_view(), name='excluir_matriz'),
 ]
