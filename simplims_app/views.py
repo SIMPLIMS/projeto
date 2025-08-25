aa417f721c7dc14775f86eaff9e609e5e463f963
DevSIMPLIMS <tcc2nanuvem@gmail.com>
2025-08-25 02:36:53 -0400

    conserta data

--- simplims_app/views.py
+++ simplims_app/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render
-import calendar
+import calendar, locale
 from calendar import HTMLCalendar
 from datetime import datetime, timedelta
+
+locale.setlocale(locale.LC_TIME, 'pt_BR.UTF-8')
 
 def home(request, ano=datetime.now().year, mes=datetime.now().strftime('%B')):
     nome = "Andrimarciely"
     mes = mes.capitalize()
 
     # converte o nome do mês em número correspondente
-    numero_mes = list(calendar.month_name).index(mes)
-    numero_mes = int(numero_mes)
+   # numero_mes = list(calendar.month_name).index(mes)
+   # numero_mes = int(numero_mes)
 
     # Create a calendar
-    cal = HTMLCalendar().formatmonth(ano, numero_mes, withyear = True)
+  #  cal = HTMLCalendar().formatmonth(ano, numero_mes, withyear = True)
 
     # Get current year
     agora = datetime.now()
+    mes = agora.strftime('%B')
     ano_corrente = agora.year
 
     # Get current time
@@ -25,8 +28,8 @@ def home(request, ano=datetime.now().year, mes=datetime.now().strftime('%B')):
         "nome": nome,
         "ano": ano,
         "mes": mes,
-        "numero_mes": numero_mes,
-        "cal": cal,
+#       "numero_mes": numero_mes,
+#       "cal": cal,
         "ano_corrente": ano_corrente,
         "horario": horario,
         })
