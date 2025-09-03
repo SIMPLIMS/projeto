6d0f3fa3c77e9550974305852d5da5e6e4d636a8
DevSIMPLIMS <tcc2nanuvem@gmail.com>
2025-09-02 20:18:36 -0400

    cria model e menu no navbar para Servico

--- simplims_app/admin.py
+++ simplims_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Matriz, Empresa, Parametro
+from .models import Matriz, Empresa, Parametro, Servico
 
 # Register your models here.
 admin.site.register(Matriz)
 admin.site.register(Empresa)
 admin.site.register(Parametro)
+admin.site.register(Servico)
--- simplims_app/models.py
+++ simplims_app/models.py
@@ -89,3 +89,18 @@ class Parametro(models.Model):
 
     class Meta:
         verbose_name = "Parametro"
+
+
+class Servico(models.Model):
+    descricao = models.CharField(max_length=100, unique=True)
+
+    matriz = models.ForeignKey(
+        "Matriz",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.descricao
+
+    class Meta:
+        verbose_name = "Serviço"
