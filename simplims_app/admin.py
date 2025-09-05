f09d2bd1eb9fbd1bcf5a3afe990bf40b64aa04e5
DevSIMPLIMS <tcc2nanuvem@gmail.com>
2025-09-04 20:33:27 -0400

    registra OrdemServico no admin

--- simplims_app/admin.py
+++ simplims_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Matriz, Empresa, Parametro, Servico
+from .models import Matriz, Empresa, Parametro, Servico, OrdemServico
 
 # Register your models here.
 admin.site.register(Matriz)
 admin.site.register(Empresa)
 admin.site.register(Parametro)
 admin.site.register(Servico)
+admin.site.register(OrdemServico)
