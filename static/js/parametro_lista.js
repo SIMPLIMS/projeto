aa555dae82ec35a0e8c56e877abbe67467abf543
DevSIMPLIMS <tcc2nanuvem@gmail.com>
2025-09-15 01:14:42 -0400

    alteração da função de listagem de parâmetro para arquivo js seoarado

---
+++ static/js/parametro_lista.js
@@ -0,0 +1,9 @@
+// simplims_app/static/simplims_app/js/parametro_lista.js
+$(document).ready(function () {
+  $('#parametros-table').DataTable({
+    "language": {
+      "url": "//cdn.datatables.net/plug-ins/1.13.6/i18n/pt-BR.json"
+    },
+    "pageLength": 10
+  });
+});
