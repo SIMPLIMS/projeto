db95d705aacdba56fe674d0d7614c3b9e42db76e
DevSIMPLIMS <tcc2nanuvem@gmail.com>
2025-09-16 14:31:30 -0400

    atualização de paginação de parametro

--- static/js/parametro_lista.js
+++ static/js/parametro_lista.js
@@ -1,9 +1,13 @@
-// simplims_app/static/simplims_app/js/parametro_lista.js
 $(document).ready(function () {
-  $('#parametros-table').DataTable({
-    "language": {
-      "url": "//cdn.datatables.net/plug-ins/1.13.6/i18n/pt-BR.json"
-    },
-    "pageLength": 10
-  });
+    $('#parametros-table').DataTable({
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.13.6/i18n/pt-BR.json"
+        },
+        "pageLength": 10,
+        "lengthChange": true,
+        "searching": true,
+        "ordering": true,
+        "info": true,
+        "paging": true
+    });
 });
